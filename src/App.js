ad4e2db008fb7d760997e8c93812d6b507a29883
Anton_Kuts <anton.kuts@intive.com>
2023-05-21 19:07:13 +0300

    not work(

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 // pages
 import { SummaryForm } from "./pages/summary/SummaryForm";
+import Options from "./pages/entry/Options";
 
 function App() {
   return (
     <div className="App">
+      <Options optionType="scoops" />
       <SummaryForm />
     </div>
   );
