74e2e3ec99e058dc111ad6ba3cdbb86db7876dc2
Anton_Kuts <anton.kuts@intive.com>
2023-05-21 20:20:11 +0300

    topping option - not work

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "../../../test-utils/testing-library-utils";
+import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
@@ -12,4 +12,22 @@ test("displays image for each scoop option from server", async () => {
   // @ts-ignore
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Displays image for each toppings option from server", async () => {
+  // Mock Service Worker will return three toppings from server
+  render(<Options optionType="toppings" />);
+
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // check the actual alt text for the images
+  // @ts-ignore
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
