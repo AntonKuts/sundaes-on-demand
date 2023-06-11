cea0d1ac96b136f5f8a9f31d9896a6b322342a8b
Anton_Kuts <anton.kuts@intive.com>
2023-06-11 15:40:28 +0300

    status 500 - test ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 // pages
 import { SummaryForm } from "./pages/summary/SummaryForm";
-import Options from "./pages/entry/Options";
+// import Options from "./pages/entry/Options";
 
 function App() {
   return (
     <div className="App">
-      <Options optionType="scoops" />
-      <Options optionType="toppings" />
       <SummaryForm />
     </div>
   );
