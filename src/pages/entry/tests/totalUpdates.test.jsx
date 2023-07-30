107007d6c2b6a8316fcd849fab76f3491a27929e
Anton_Kuts <anton.kuts@intive.com>
2023-07-30 19:33:03 +0300

    topping option test ready

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
-
-test("subtotal starts at 0", async () => {
+ 
+// scoops tests
+test("subtotal scoops starts at 0", async () => {
   const { unmount } = render(<Options optionType="scoops" />);
 
   // make sure total starts out $0.00
@@ -37,4 +38,40 @@ test("update scoop subtotal when scoops change", async () => {
   await user.clear(chocolateInput);
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+// toppings tests
+test("subtotal toppings starts at 0", async () => {
+  const { unmount } = render(<Options optionType="toppings" />);
+
+  // make sure total starts out $0.00
+  const scoopsSubtotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // explicitly unmount component to trigger network call cancellation on cleanup
+  // (necessary to avoid race condition if component unmounts when test function exits)
+  unmount();
+});
+
+test("update scoop subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out $0.00
+  const scoopsSubtotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // update M&Ms-topping-checkbox scoops to 1 and check the subtotal
+  const MAndMstopping = await screen.findByRole("checkbox", {
+    name: "M&Ms",
+  });
+  const Cherriestopping = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(MAndMstopping);
+  expect(scoopsSubtotal).toHaveTextContent("1.50");
+  await user.click(Cherriestopping);
+  expect(scoopsSubtotal).toHaveTextContent("3.00");
+  await user.click(MAndMstopping);
+  expect(scoopsSubtotal).toHaveTextContent("1.50");
 });
